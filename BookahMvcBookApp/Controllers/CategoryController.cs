0ff71b1aad942c66d71735ab4aa600d505cc1fc9
Morena-Leluma <lelumaromeo@gmail.com>
2021-01-17 17:50:12 +0200

    210034408 - 17 Jan 21 - Created: - Created a login and registration module.

---
+++ BookahMvcBookApp/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookahMvcBookApp.Models;
+
+namespace BookahMvcBookApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        dbBookahMvcBookAppEntities db = new dbBookahMvcBookAppEntities();
+
+        public ActionResult Index()
+        {
+            var query = db.tblCategories.ToList();
+            return View(query);
+        }
+        // GET: Category
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tblCategory c)
+        {
+            if(ModelState.IsValid)
+            {
+                tblCategory cat = new tblCategory();
+                cat.Name = c.Name;
+                db.tblCategories.Add(cat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["msg"] = "Not Inserted in category.";
+            }
+            return View();
+        }
+    }
+}
