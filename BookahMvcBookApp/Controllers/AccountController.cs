f0edbd0790a4e547dc1568ba80cfb9d7fbabd5e8
Diteboho <dlethoko17@gmail.com>
2021-01-20 16:06:43 +0200

    216005388 - 20 Jan 21- Updated - I fixed the Home screen to display the login module.

--- BookahMvcBookApp/Controllers/AccountController.cs
+++ BookahMvcBookApp/Controllers/AccountController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Security;
 using System.Web.Mvc;
+using System.Web.Security;
 using BookahMvcBookApp.Models;
 
 namespace BookahMvcBookApp.Controllers
@@ -11,35 +11,9 @@ namespace BookahMvcBookApp.Controllers
     public class AccountController : Controller
     {
         dbBookahMvcBookAppEntities db = new dbBookahMvcBookAppEntities();
-
-        public ActionResult Register()
+        // GET: Account
+        public ActionResult Index()
         {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Register(tblUser t)
-        {
-            tblUser u = new tblUser();
-            if(ModelState.IsValid)
-            {
-                u.Firstname = t.Firstname;
-                u.Lastname = t.Lastname;
-                u.Email = t.Email;
-                u.IDNumber = t.IDNumber;
-                u.PhoneNumber = t.PhoneNumber;
-                u.Password = t.Password;
-                u.RoleType = 2;
-                db.tblUsers.Add(u);
-                db.SaveChanges();
-
-                return RedirectToAction("Login", "Account");
-            }
-            else
-            {
-                TempData["msg"] = "You are not registered!!";
-            }
-
             return View();
         }
 
@@ -47,39 +21,35 @@ namespace BookahMvcBookApp.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public ActionResult Login(tblUser t)
         {
             var query = db.tblUsers.SingleOrDefault(m => m.Email == t.Email && m.Password == t.Password);
-            if(query != null)
-            {             
-                if(query.RoleType == 1)
+            if (query != null)
+            {
+                if (query.RoleType == 1)
                 {
+                    Session["uid"] = query.UserId;
                     FormsAuthentication.SetAuthCookie(query.Email, false);
-                    Session["User"] = query.Firstname;
+                    Session["User"] = query.Email;
+                    return RedirectToAction("Index", "Home");
+                }
+                else if (query.RoleType == 2)
+                {
+                    Session["uid"] = query.UserId;
+                    FormsAuthentication.SetAuthCookie(query.Email, false);
+                    Session["User"] = query.Email;
                     return RedirectToAction("Index", "Home");
                 }
 
-                else if (query.RoleType == 2)
-                {
-                    FormsAuthentication.SetAuthCookie(query.Email, false);
-                    Session["User"] = query.Firstname;
-                    return RedirectToAction("Index", "Home");
-                }
             }
             else
             {
                 TempData["msg"] = "Invalid Username or Password";
             }
+
             return View();
         }
 
-        public ActionResult Signout()
-        {
-            FormsAuthentication.SignOut();
-            return RedirectToAction("Index", "Home");
-
-        }
     }
 }
